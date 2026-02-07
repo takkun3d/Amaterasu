c3c7aea801594260bf78a6368f569272aa742039
takkun <229980073+takkun3d@users.noreply.github.com>
2026-02-08 01:50:17 +0900

    feat: initial commit

---
+++ amaterasu/scripts/amaterasu/rendering/noice_manager.py
@@ -0,0 +1,114 @@
+# ==============================================================================
+#
+# Noice Manager
+#
+# ==============================================================================
+from __future__ import annotations
+from typing import TYPE_CHECKING
+import logging
+
+try:
+    from PySide2.QtCore import Qt, Slot
+    from PySide2.QtWidgets import QWidget
+
+except ImportError:
+    if not TYPE_CHECKING:
+        from PySide6.QtCore import Qt, Slot
+        from PySide6.QtWidgets import QWidget
+from ..lib import parser, widgets
+
+
+# ==============================================================================
+#
+# Variables
+#
+# ==============================================================================
+__product__: str = 'Noice Manager'
+__version__: str = '1.00'
+__doc__ = 'Easy tool to control Arnold Noice. Check images and export batch.'
+__copyright__ = (
+    'Copyright (c) 2014-2026 takkun (takkun3d). Released under the MIT License.'
+)
+_logger: logging.Logger = logging.getLogger(__product__)
+
+
+# ==============================================================================
+#
+# Classes
+#
+# ==============================================================================
+class Settings(parser.ToolSettings):
+    '''Settings for tool.'''
+
+    window_geo: parser.Variant[str] = parser.Variant('')
+
+
+class MainWindow(widgets.ToolWidget):
+    '''Tool main window'''
+
+    def __init__(
+        self,
+        parent: QWidget | None = None,
+        flag: Qt.WindowFlags = Qt.WindowFlags(),
+    ) -> None:
+        '''Initialize widget.'''
+        super().__init__(parent, flag)
+        self.setWindowTitle(__product__)
+        self.resize(400, 200)
+
+        option_widget: QWidget = self.option_widget()
+        option_widget.setObjectName('')
+
+    # override
+    def load_settings(self) -> None:
+        '''Load ui settings from file.[override]'''
+        settings: Settings = Settings.instance(__name__, True)
+        self.restoreGeometry(widgets.to_qt(settings.window_geo.value()))
+        self.set_valid_options()
+
+    # override
+    def save_settings(self) -> None:
+        '''Save ui settings to file.[override]'''
+        settings: Settings = Settings.instance(__name__, True)
+        settings.window_geo.set_value(widgets.to_ascii(self.saveGeometry()))
+        settings.write()
+
+    # override
+    def reset_settings(self) -> None:
+        '''Reset ui settings.[override]'''
+        settings: Settings = Settings.instance(__name__, True)
+        settings.reset()
+        self.load_settings()
+
+    # override
+    def about(self) -> None:
+        '''Show a about dialog.[override]'''
+        widgets.AboutDialog.info(
+            self, __product__, __version__, __copyright__, __doc__
+        )
+
+    @Slot()
+    def set_valid_options(self) -> None:
+        '''Synchronize with valid options.'''
+
+    @widgets.undo
+    def apply(self) -> None:
+        '''Apply'''
+        self.save_settings()
+        main()
+
+
+# ==============================================================================
+#
+# Functions
+#
+# ==============================================================================
+def apply() -> bool:
+    '''Docstring'''
+    return True
+
+
+def main() -> None:
+    '''Show window.'''
+    window: MainWindow = MainWindow()
+    window.show()
