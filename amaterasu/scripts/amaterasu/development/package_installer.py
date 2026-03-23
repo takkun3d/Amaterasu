e9bc38307b80b5e83eb26463f59ac2461511c315
takkun <229980073+takkun3d@users.noreply.github.com>
2026-03-23 21:20:52 +0900

    refactor(development): switch to custom logger for toast notifications

--- amaterasu/scripts/amaterasu/development/package_installer.py
+++ amaterasu/scripts/amaterasu/development/package_installer.py
@@ -5,7 +5,6 @@
 # ==============================================================================
 from __future__ import annotations
 from typing import TYPE_CHECKING
-import logging
 import sys
 import os
 import subprocess
@@ -41,7 +40,7 @@ except ImportError:
             QMessageBox,
         )
 
-from ..lib import parser, widgets
+from ..lib import logger, parser, widgets
 
 
 # ==============================================================================
@@ -57,7 +56,7 @@ __doc__ = (
 __copyright__ = (
     'Copyright (c) 2014-2026 takkun (takkun3d). Released under the MIT License.'
 )
-_logger: logging.Logger = logging.getLogger(__product__)
+_logger: logger.Logger = logger.get_logger(__product__)
 
 PACKAGE_MAP: dict[str, str] = {
     'numpy': 'numpy',
