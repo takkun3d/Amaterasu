3a0c7bd1dfa8a7ba4504a640546b0dc84ac45ec3
takkun <229980073+takkun3d@users.noreply.github.com>
2026-05-03 18:19:16 +0900

    refactor: migrate cleanup tools to new DCC base and streamline logging

--- amaterasu/scripts/amaterasu/file/remove_node_editor_info.py
+++ amaterasu/scripts/amaterasu/file/remove_node_editor_info.py
@@ -1,50 +1,42 @@
-# ==============================================================================
+# Copyright (c) 2014-2026 takkun (takkun3d). Released under the MIT License.
 #
-# Remove Node Editor Info
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# ==============================================================================
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""Removes Node Editor Info nodes from the open scene.
+
+This tool finds and deletes all 'nodeGraphEditorInfo' nodes, which
+often accumulate in scenes, bloat file sizes, and cause unnecessary
+warnings.
+"""
+
 from __future__ import annotations
-from maya import cmds
-from ..lib import logger
+from amaterasu.base import dcc, utils
+
+__product__: str = "Remove Node Editor Info"
+__version__: str = "1.10"
+_logger: utils.Logger = utils.get_logger(__product__)
 
 
-# ==============================================================================
-#
-# Variables
-#
-# ==============================================================================
-__product__: str = 'Remove Node Editor Info'
-__version__: str = '1.00'
-__doc__ = 'Remove Node Editor Info from an open scene.'
-__copyright__ = (
-    'Copyright (c) 2014-2026 takkun (takkun3d). Released under the MIT License.'
-)
-_logger: logger.Logger = logger.get_logger(__product__)
-
-
-# ==============================================================================
-#
-# Classes
-#
-# ==============================================================================
-
-
-# ==============================================================================
-#
-# Functions
-#
-# ==============================================================================
 def main() -> None:
-    '''Do it.'''
-    nodes: list[str] = cmds.ls(type='nodeGraphEditorInfo')
-    for node in nodes:
-        # dst, src, dst, src ...
-        connections: list[str] = (
-            cmds.listConnections(node, connections=True, plugs=True) or []
-        )
-        for dst_plug, src_plug in zip(connections[::2], connections[1::2]):
-            cmds.disconnectAttr(src_plug, dst_plug)
+    """Executes the removal of nodeGraphEditorInfo nodes.
 
-        cmds.delete(node)
-
-    _logger.info('Done.')
+    Delegates the safe disconnection and deletion of these nodes to
+    the base DCC scene utilities, then logs the resulting status.
+    """
+    result: utils.Result = dcc.scene.remove_node_editor_info()
+    result.log(_logger)
--- amaterasu/scripts/amaterasu/file/remove_unknown_plugins.py
+++ amaterasu/scripts/amaterasu/file/remove_unknown_plugins.py
@@ -1,44 +1,42 @@
-# ==============================================================================
+# Copyright (c) 2014-2026 takkun (takkun3d). Released under the MIT License.
 #
-# Remove Unknown Plugins
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# ==============================================================================
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""Removes unknown plugins from the open scene.
+
+This tool identifies plugins that are required by the scene but are
+not currently loaded or available in the environment, and removes
+their requirements from the file to prevent loading errors.
+"""
+
 from __future__ import annotations
-from maya import cmds
-from ..lib import logger
+from amaterasu.base import dcc, utils
+
+__product__: str = "Remove Unknown Plugins"
+__version__: str = "1.10"
+_logger: utils.Logger = utils.get_logger(__product__)
 
 
-# ==============================================================================
-#
-# Variables
-#
-# ==============================================================================
-__product__: str = 'Remove Unknown Plugins'
-__version__: str = '1.01'
-__doc__ = 'Remove unknown plugins from an open scene.'
-__copyright__ = (
-    'Copyright (c) 2014-2026 takkun (takkun3d). Released under the MIT License.'
-)
-_logger: logger.Logger = logger.get_logger(__product__)
-
-
-# ==============================================================================
-#
-# Classes
-#
-# ==============================================================================
-
-
-# ==============================================================================
-#
-# Functions
-#
-# ==============================================================================
 def main() -> None:
-    '''Do it.'''
-    unknown_plugins: list[str] = cmds.unknownPlugin(query=True, list=True) or []
-    for plugin in unknown_plugins:
-        cmds.unknownPlugin(plugin, remove=True)
-        _logger.info('Remove plugin : %s', plugin)
+    """Executes the removal of unknown plugins.
 
-    _logger.info('Done.')
+    Delegates the cleanup of unavailable plugin requirements to the
+    base DCC plugin utilities, then logs the resulting status.
+    """
+    result: utils.Result = dcc.plugin.remove_unknown()
+    result.log(_logger)
